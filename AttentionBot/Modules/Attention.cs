3da186f8bf18ae1fba813cf06b8ad8ad5faa9864
HallBenjamin <hallb1016@gmail.com>
2017-08-23 17:12:28 -0400

    Added C# Form
    C# is EXPERIMENTAL

---
+++ AttentionBot/Modules/Attention.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttentionBot.Modules
+{
+    public class Attention : ModuleBase<SocketCommandContext>
+    {
+        [Command(null)]
+        // TODO: Fix command, add randomizer
+        public async Task attention()
+        {
+            await Context.Channel.SendMessageAsync("Attention to A3");
+        }
+    }
+}
