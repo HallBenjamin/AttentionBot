96acc531b6736e081f8f447d3f56f751cafdd4eb
HallBenjamin <hallb1016@gmail.com>
2017-08-27 20:51:52 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace AttentionBot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+            => new Program().StartAsync().GetAwaiter().GetResult();
+
+        public static void endMain()
+            => new Program().StopAsync().GetAwaiter();
+
+        public String token = "Removed for Security";
+
+        private DiscordSocketClient _client;
+
+        private CommandHandler _handler;
+        public async Task StartAsync()
+        {
+            Console.Title = "Attention! Bot for Discord";
+
+            _client = new DiscordSocketClient();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+
+            _handler = new CommandHandler(_client);
+
+            Console.WriteLine("Attention! Bot Online");
+
+            await Task.Delay(-1);
+        }
+        public async Task StopAsync()
+        {
+            Console.WriteLine("Attention! Bot Offline");
+
+            await _client.LogoutAsync();
+
+            await _client.StopAsync();
+        }
+    }
+}
