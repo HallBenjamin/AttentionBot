9be4ffdb86098310ca67f82edde0a7a88db008ce
Benjamin Hall <hallb1016@gmail.com>
2019-06-25 02:07:27 -0400

    Update SecurityInfo.cs for Pipeline

--- SecurityInfo.cs
+++ SecurityInfo.cs
@@ -6,7 +6,7 @@ namespace AttentionBot
     {
         public const string token = "Insert Bot Security Token here";
         public const string botID = "Insert Bot Tag/ID here";
-        public const ulong botClient = Insert Bot Client ID here;
+        public const ulong botClient = 0; // Insert Bot Client ID here
         public const string botName = "Insert Bot Name here";
         public const string botVersion = "Insert Bot Version here";
         public static readonly Color botColor = new Color(0, 90, 140);
